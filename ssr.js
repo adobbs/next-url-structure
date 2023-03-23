64269c84aa286d25a8bda0a078889f2e77e8cf9d
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-23 10:31:26 -0400

    Add ssq and ssr pattern examples

---
+++ ssr.js
@@ -0,0 +1,29 @@
+// pages/blog/[slug].js
+
+import Head from 'next/head';
+function BlogPost({ blog }) {
+  return (
+    <div>
+      <Head>
+        <title>{blog.title} | My Site</title>
+      </Head>
+      <div>
+        <h1>{blog.title}</h1>
+        <p>{blog.text}</p>
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const res = await fetch(`https://www.example.com/api/blog/${query.slug}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+}
+
+export default BlogPost;
